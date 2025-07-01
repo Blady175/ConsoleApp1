ee95f5624310ce3ae29cc1177877946ae4c8ba17
Bladimir Benavides <bladimirbenavides0@gmail.com>
2025-07-01 12:39:03 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Bodega.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Bodega
+    {
+        public string Nombre { get; set; }
+        public string Nombre2 { get; set; }
+        public string Nombre3 { get; set; }
+
+    }
+}
