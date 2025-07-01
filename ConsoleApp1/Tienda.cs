39f4cad4222bb5c132343748842846743cd6c6fe
Bladimir Benavides <bladimirbenavides0@gmail.com>
2025-07-01 12:40:26 -0500

    Agregar Cmabios de teinda

---
+++ ConsoleApp1/Tienda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Tienda
+    {
+        public int id;
+        public string Nombre { get; set; }
+        public string Title { get; set; }
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+        public string Url { get; set; }
+        public string Url2 { get; set; }
+
+    }
+}
